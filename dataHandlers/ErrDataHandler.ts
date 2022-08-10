ef4b522f1f04a48275a9c2b4460307dd561e84f4
water <2778335106@qq.com>
2022-08-10 21:54:03 +0800

    增加错误时发送用户行为, 重复错误不会二次上报

--- dataHandlers/ErrDataHandler.ts
+++ dataHandlers/ErrDataHandler.ts
@@ -1,19 +1,26 @@
-import { category } from './../client';
 import { errRecord } from '../monitors/monitorJsErr';
 import { Client, Plugin } from '../client';
 
 export const ErrDataHandler = (client: Client): Plugin => {
+  const errUids: Array<string> = [];
+  const errDataBuild = (_data: errRecord) => {
+    const data = {
+      ..._data,
+      breadcrumbs: client.breadcrumbs.get(),
+    };
+    client.breadcrumbs.clear();
+    errUids.push(data.errUid);
+    return data;
+  };
   return {
     beforeBuildData: (_data) => {
       let data = _data as errRecord;
-      if (data.category === 'error') { 
-        data = {
-          ...data,
-          breadcrumbs: client.breadcrumbs,
-        } as any
+      if (data.category === 'error') {
+        if (!errUids.includes(data.errUid)) {
+          data = errDataBuild(data);
+        }
       }
-      client.breadcrumbs.clear;
-      return data
+      return data;
     },
   };
 };
